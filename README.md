c4274df45357d7f31a98f92c9a6b2a52b0394d85
ikz87 <iikz87ii@gmail.com>
2022-12-03 14:15:47 -0600

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm doing this for fun, not sure wether I'll finish the whole month or not.
+
+My goal with this is to come up with cool solutions rather than fast ones
---
+++ day2/part2.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# rock = a, 1 point
+# paper = b, 2 points
+# scissors = c, 3 points
+# X = lose
+# Y = draw 
+# Z = win
+
+parse () {
+    case $1 in
+        A | X )
+            echo "0"
+            ;;
+        B | Y )
+            echo "1"
+            ;;
+        C | Z )
+            echo "2"
+            ;;
+    esac
+}
+
+score=0
+while read line
+do
+    other=`parse ${line%% *}`
+    result=`parse ${line##* }`
+    self=$(( (other + result + 2) % 3 ))
+    roundpoints=$(( self + 1 + 3 * result ))
+    score=$(( score + roundpoints ))
+    echo "current score is $score"
+
+done < ./input;
